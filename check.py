b6fbc97bdb3a7f191d9dcee1ca2e0784b51c61e5
dhruvramani <dhruvramani98@gmail.com>
2016-08-07 15:23:43 +0530

    Added Code

---
+++ check.py
@@ -0,0 +1,26 @@
+import os
+
+dirF = list()
+def checkDir(originalDir, backupDir):
+    lsOut = os.popen('ls -a {}'.format(originalDir)).read().split()
+    lsBackOut = os.popen('ls -a {}'.format(backupDir)).read().split()
+    print('Scanning Through {}'.format(originalDir))
+    
+    for i in lsOut[2:]:
+        if os.path.isdir('{}/{}'.format(originalDir,i)):
+            if i in lsBackOut:
+                checkDir('{}/{}'.format(originalDir, i), '{}/{}'.format(backupDir, i)) 
+            else :
+                dirF.append('\033[1m\033[92m==>\033[0m Directory \033[4m{}/{}\033[0m Needs to be Backed-up\033[0m'.format(originalDir,i))
+        elif os.path.isfile('{}/{}'.format(originalDir,i)):
+            if i not in lsBackOut:
+                dirF.append('\033[1m\033[94m==>\033[0m File \033[4m{}/{}\033[0m Needs to be Backed-up\033[0m'.format(originalDir,i))
+
+def main():
+    dir = '{}/{}'.format(os.getcwd(), input('Enter Relative Directory : '))
+    backupDir = '/Volumes/{}'.format(input('Enter Backup_Disk/Directory_Name : '))
+    checkDir(dir, backupDir)
+    print('\n' + '\n'.join(dirF))
+
+if __name__ == '__main__':
+    main()
